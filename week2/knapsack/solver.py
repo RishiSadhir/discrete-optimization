fb8b99d329261240ba0a5dbaf86f0200c5f38c9f
Rishi <rishi.sadhir@gmail.com>
2019-08-29 21:27:43 -0400

    Switching distros

--- week2/knapsack/solver.py
+++ week2/knapsack/solver.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 from collections import namedtuple
 
 Item = namedtuple("Item", "idx weight value")
@@ -7,22 +5,154 @@ Item = namedtuple("Item", "idx weight value")
 _global_cache = {}
 _items = []
 
-def solve_it(input_data):
-    # Modify this code to run your optimization algorithm
+def solve_it_real(input_data):
     input = parse_input(input_data.strip())
-    #
+
     if input['item_count'] <= 200:
-        optim, conf, decisions = solve_by_dynamic(input['item_count'], input['capacity'], input['values'],
+        optim, conf, decisions = solve_by_dynamic(input['item_count'],
+                                                  input['capacity'],
+                                                  input['values'],
+                                                  input['weights'])
+    elif input['item_count'] >= 1000:
+        optim, conf, decisions = solve_by_density(input['item_count'],
+                                                  input['capacity'],
+                                                  input['values'],
                                                   input['weights'])
     else:
-        optim, conf, decisions = solve_by_density(input['item_count'], input['capacity'], input['values'],
-                                                    input['weights'])
+        optim, conf, decisions = solve_by_density(input['item_count'],
+                                                  input['capacity'],
+                                                  input['values'],
+                                                  input['weights'])
     return format_output(optim, conf, decisions)
+
+
+def solve_it(input_data):
+    print("Solving with lr bab")
+    input = parse_input(input_data.strip())
+    bab = BranchAndBound(input)
+    bab.linear_relaxation_dfs()
+    bab.build_decisions()
+    return bab.format_output()
+
+def solve_it_dp(input_data):
+    input = parse_input(input_data.strip())
+    print("Solving with dynamic programming")
+    optim, conf, decisions = solve_by_dynamic(input['item_count'],
+                                                  input['capacity'],
+                                                  input['values'],
+                                                  input['weights'])
+    return format_output(optim, conf, decisions)
+
+################################
+## Branch and Bound
+################################
+
+class BranchAndBound:
+    global_optimum = 0
+    items = []
+    items_by_density = []
+    decisions = None
+    knapsack = ()
+    capacity = 0
+
+    def __init__(self, input):
+        self.global_optimum = 0
+        idx = [x for x in range(0, len(input['values']))]
+        self.items = [Item(i, w, v) for i, w, v in
+                      zip(idx, input['weights'], input['values'])]
+        self.decisions = [0] * len(self.items)
+        self.knapsack = ()
+        self.capacity = input['capacity']
+        self.explored = 0
+
+    def linear_relaxation_dfs(self):
+        self._sort_items_by_density()
+        self._linear_relaxation_dfs(0, self.capacity, tuple(), 0)
+        self.explored = 1
+
+    def _linear_relaxation_dfs(self, value=0, room=None, knapsack = tuple(), i=0):
+        if room == None:
+            room = self.capacity
+        if i >= len(self.items_by_density) or room == 0:
+            if value > self.global_optimum:
+                self.global_optimum = value
+                self.knapsack = knapsack
+            return
+        elif self._optimistic_evaluation(i, room, value) <= self.global_optimum:
+            return
+        elif self.items_by_density[i].weight > room:
+            self._linear_relaxation_dfs(value, room, knapsack, i+1)
+            return
+        else:
+            self._linear_relaxation_dfs(value, room, knapsack, i+1)
+            self._linear_relaxation_dfs(value + self.items_by_density[i].value,
+                                        room - self.items_by_density[i].weight,
+                                        knapsack + (self.items_by_density[i],),
+                                        i+1)
+
+    def _optimistic_evaluation(self, idx, capacity, starting_value = 0):
+        optimistic_estimate = starting_value
+        for item in self.items_by_density[idx:]:
+            if capacity - item.weight > 0:
+                capacity -= item.weight
+                optimistic_estimate += item.value
+            else:
+                fraction = capacity / item.weight
+                optimistic_estimate += fraction * item.value
+                break
+        return optimistic_estimate
+
+    def _sort_items_by_density(self):
+        densities = [x.value/x.weight for x in self.items]
+        self.items_by_density = [item for _,item in
+                                 sorted(zip(densities, self.items), reverse = True)]
+
+    def exhaustive_dfs(self):
+        self._exhaustive_dfs(0, self.capacity, 0, tuple(), 0)
+        self.explored = 1
+
+    def _exhaustive_dfs(self, value, room, estimate, knapsack, i):
+        if i >= len(self.items) or room == 0:
+            if value > self.global_optimum:
+                self.global_optimum = value
+                self.knapsack = knapsack
+                return
+        elif self.items[i].weight > room:
+            self._exhaustive_dfs(value, room, estimate, knapsack, i+1)
+            return
+        else:
+            self._exhaustive_dfs(value, room, estimate, knapsack, i+1)
+            self._exhaustive_dfs(value + self.items[i].value,
+                                 room - self.items[i].weight,
+                                 estimate,
+                                 knapsack + (self.items[i],),
+                                 i+1)
+
+    def build_decisions(self):
+        if not self.explored:
+            raise ValueError("Havent explored search space.")
+        self.decisions = [0] * len(self.items)
+        for item in self.knapsack:
+            self.decisions[item.idx] = 1
+
+    def format_output(self):
+        output_data = str(self.global_optimum) + " " + str(1) + "\n"
+        output_data += ' '.join(map(str, self.decisions))
+        return output_data
+
+    def __str__(self):
+        str = f"Global Optimum: {self.global_optimum}\n"
+        str += f"Knapsack: {self.knapsack}\n"
+        str += f"Decisions: {self.decisions}"
+        return str
+
+    def __repr__(self):
+        return str(self)
 
 
 ################################
 ## Dynamic Programming
-###############################
+################################
 
 def solve_by_dynamic(n, k, v, w):
     # Build a list of structs for easy access
@@ -176,7 +306,6 @@ if __name__ == '__main__':
     else:
         print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)')
 
-
 ################################
 ## Tests
 ################################
@@ -192,6 +321,13 @@ def get_test_input():
     input = parse_input(read_input(file))
     return input
 
+def get_known_input():
+    return {
+        'item_count': 4,
+        'capacity': 7,
+        'values': [16, 19, 23, 28],
+        'weights': [2, 3, 4, 5]}
+
 def test_known():
     known = {
         'item_count': 4,
@@ -204,3 +340,14 @@ def test_known():
         known['values'],
         known['weights'])
     return result
+
+def test_branch_and_bound():
+    input = {
+        'item_count': 4,
+        'capacity': 7,
+        'values': [16, 19, 23, 28],
+        'weights': [2, 3, 4, 5]}
+    bab = BranchAndBound(input)
+    bab.exhaustive_dfs()
+    bab.build_decisions()
+    return bab
